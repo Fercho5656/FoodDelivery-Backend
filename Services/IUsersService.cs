cdf26b767bea082ee7de999a3540083ace6bd8ff
Fercho5656 <morenomoranfernando@gmail.com>
2022-04-24 02:06:55 -0500

    Added method to return user by email

--- Services/IUsersService.cs
+++ Services/IUsersService.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
 using FoodDelivery_Backend.Base;
 using FoodDelivery_Backend.Models.Base;
 
 namespace FoodDelivery_Backend.Services {
     public interface IUsersService : IEntityBaseRepository<User> {
+        Task<User> GetByEmail(string email);
     }
 }
--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -1,9 +1,19 @@
+using System.Threading.Tasks;
 using FoodDelivery_Backend.Base;
 using FoodDelivery_Backend.Context;
 using FoodDelivery_Backend.Models.Base;
-
+using Microsoft.EntityFrameworkCore;
 namespace FoodDelivery_Backend.Services {
     public class UsersService : EntityBaseRepository<User>, IUsersService {
-        public UsersService(AppDbContext context) : base(context) { }
+        private readonly AppDbContext _context;
+        public UsersService(AppDbContext context) : base(context) {
+            _context = context;
+        }
+
+        public async Task<User> GetByEmail(string email) {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null) return null;
+            return await Get(user.Id, u => u.Role);
+        }
     }
 }
