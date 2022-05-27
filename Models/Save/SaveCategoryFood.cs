13be7343f448f2ab515f9d05c41a54305a640343
Fercho5656 <morenomoranfernando@gmail.com>
2022-05-27 02:05:16 -0500

    Category Food Models

---
+++ Models/Save/SaveCategoryFood.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace FoodDelivery_Backend.Models.Save {
+    public class SaveCategoryFood {
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/ViewModel/VMCategoryFood.cs
@@ -0,0 +1,6 @@
+namespace FoodDelivery_Backend.Models.ViewModel {
+    public class VMCategoryFood {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
